8711a1dcabf8885e38702d4fe76c2a273c7ffede
Meng <z_meng@ats-hr420-dm05.fc.ca>
2020-02-28 12:46:15 -0500

    adding sound

--- js/main.js
+++ js/main.js
@@ -84,8 +84,21 @@
             }
 
             // create and play explosion sound
-            
-          }
+            let audio=document.createElement('audio');
+
+
+            audio.addEventListener("ended",function(){
+              document.body.removeChild(audio);
+            });//when done,delete
+
+            audio.src="audio/explode.mp3";//sets the audio source
+            document.body.appendChild(audio);//add it to the page
+
+            audio.play();
+
+           //end explosion sound
+
+        }
         });
 
         bullet.y -= bullet.speed;
@@ -123,7 +136,7 @@
       window.requestAnimationFrame(draw);
   }
 
-  
+
   function createBullet() {
     // create / draw a bullet and push it into the bullet array
     let newBullet = {
@@ -137,7 +150,17 @@
     bullets.push(newBullet);
 
     // create and play cheesy laser sound
-    
+     let audio=document.createElement('audio');
+     //create an<audio></audio> element
+
+     audio.addEventListener("ended",function(){
+       document.body.removeChild(audio);
+     });//when done,delete
+
+     audio.src="audio/laser.mp3";//sets the audio source
+     document.body.appendChild(audio);//add it to the page
+
+     audio.play();
   }
 
   function movePlayer(e) {
